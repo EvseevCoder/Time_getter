e2e349ca6073ed32211cc31f4494ecb34b3de789
EvseevCoder <andrey.semin.evseev@gmail.com>
2021-06-13 15:09:29 +0300

    написание вывода актуального времени в консоль

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello, world");
+Now = new Date();
+
+let Hours = Now.getHours() // нахождение актуального времени 
+let Minutes = Now.getMinutes()
+let Seconds = Now.getSeconds()
+
+if (Hours < 10) { // Добавление нуля перед числеми меньше десяти
+    Hours = '0' + Hours
+}
+
+if (Minutes < 10) {
+    Minutes = '0' + Minutes
+}
+
+if (Seconds < 10) {
+    Seconds = '0' + Seconds
+}
+
+
+console.log(Hours + ':' + Minutes + ':' + Seconds);
